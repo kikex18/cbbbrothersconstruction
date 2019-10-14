755f3e22c4edc77fe1f6bb27a8005ce9f39c6a9e
Enrique Moreno <kikex2.8@gmail.com>
2019-10-14 14:40:54 -0600

    logo cambiado y estructura hacia arreglos en la conexión

--- conexion.php
+++ conexion.php
@@ -1,5 +1,6 @@
 <?php
-$link = mysqli_connect("localhost", "aa_000taq", "N-4-nRu?KAdX", "aa_000taq");
+//$link = mysqli_connect("localhost", "aa_000taq", "N-4-nRu?KAdX", "aa_000taq");
+$link = mysqli_connect("localhost", "root", "", "bd_testimonials");
 
 
 if (!$link) {
@@ -8,4 +9,16 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
+
+$Testimonials = array(
+    array(
+        "Review"       => "Shirley F. in Duluth, GA",
+        "Project"      => "Repair or Partially Replace Wood or Fiber-Cement Siding",
+        "Comment"      => "On time, professional, affordable and very easy to work with.",
+        "date"         => "03-17-2019",
+        "stars"        => 5,
+        //Tipos: homeadvisor,facebook/google
+        "company"      => 'homeadvisor',
+    ), 
+);
 ?>
